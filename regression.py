c81948e76d5a4c8034f9546724368d8f67e42fda
Jorge Montanez <montanez.jorge@gmail.com>
2019-11-12 19:23:03 -0500

    Added convolutional network

--- regression.py
+++ regression.py
@@ -1,3 +1,6 @@
+import os
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+#os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
 seednumber = 8
 from numpy.random import seed
 seed(seednumber)
@@ -18,17 +21,15 @@ from sklearn.externals.joblib import dump, load
 from sklearn.decomposition import PCA
 from sklearn.utils import shuffle
 from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import load_model
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation
-from tensorflow.keras.regularizers import l2
+from tensorflow.keras import layers, backend as K
+from tensorflow.keras.models import load_model, Sequential, Model
+from tensorflow.keras.layers import Input, Concatenate, Dot, Activation, Dense, Flatten, Conv2D, BatchNormalization, MaxPooling2D, Lambda
+from tensorflow.keras.regularizers import l2, l1
 from tensorflow.python.keras.utils.data_utils import Sequence
-from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
-from tensorflow.keras.utils import CustomObjectScope
+from tensorflow.keras.utils import CustomObjectScope, get_custom_objects
+from tensorflow.keras.optimizers import Optimizer, Adam
 import numpy as np
 import random
-import os
 import sys
 import pickle
 import warnings
@@ -196,14 +197,6 @@ def TrainWithKeras():
     model.save("C:\\Users\\jxmr\\Desktop\\ProjectIII\\Data2\\kerasmodel")
 
 
-def TrainWithKerasPreprocessing(X, Y):
-    scaler = MinMaxScaler()
-    X = scaler.fit_transform(X)
-    return X, Y
-
-def EmptyPreprocessing(X, Y):
-    return X, Y
-
 def R_squared(y, y_pred):
     residual = tf.reduce_sum(tf.square(tf.subtract(y, y_pred)))
     total = tf.reduce_sum(tf.square(tf.subtract(y, tf.reduce_mean(y))))
@@ -322,8 +315,8 @@ def ValidateTraining():
         plt.show()
 
 def GenerateSegmentationData():
-    frames = open("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\frames", "ab")
-    boxes = open("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\boxes", "ab")
+    frames = open("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\frames_full", "ab")
+    boxes = open("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\boxes_full", "ab")
 
     for r, d, f in os.walk("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\testDataset"):
         for file in f:
@@ -331,8 +324,8 @@ def GenerateSegmentationData():
                 vidcap = cv2.VideoCapture(os.path.join(r, file))
                 success,image = vidcap.read()
                 while success:
-                    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                    resize = cv2.resize(gray, (int(gray.shape[1]/2), int(gray.shape[0]/2)), interpolation= cv2.INTER_LANCZOS4)
+                    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                    resize = cv2.resize(image, (int(image.shape[1]/2), int(image.shape[0]/2)), interpolation= cv2.INTER_LANCZOS4)
                     array = np.asarray(resize)
                     array.tofile(frames)  
                     success,image = vidcap.read()
@@ -345,8 +338,8 @@ def GenerateSegmentationData():
                         vector[0] = 1
                     count = 1
                     for point in frame.iter("point"):
-                        vector[count] = float(point.attrib["x"]) / 2
-                        vector[count+1] = float(point.attrib["y"]) / 2
+                        vector[count] = float(point.attrib["x"]) / 2 
+                        vector[count + 1] = float(point.attrib["y"]) / 2
                         count = count + 2
                     vector.tofile(boxes)
 
@@ -376,13 +369,13 @@ def GenerateSegmentationData():
 #    img.show()
 
 def ShuffleSegementationData():
-    X_train = np.fromfile("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\frames_processed", dtype=np.dtype("(518400,)f8"))
-    y_train = np.fromfile("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\boxes_processed", dtype=np.dtype("(9,)f8"))
+    X_train = np.fromfile("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\frames", dtype=np.dtype("(518400,)u1"))
+    y_train = np.fromfile("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\boxes", dtype=np.dtype("(9,)f8"))
 
     X_train, y_train = shuffle(X_train, y_train)
 
-    X_train.tofile("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\frames_processed_shuffled")
-    y_train.tofile("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\boxes_processed_shuffled")
+    X_train.tofile("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\frames_shuffled")
+    y_train.tofile("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\boxes_shuffled")
 
 def TrainSegmentation():
 
@@ -416,6 +409,168 @@ def TrainSegmentation():
     early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.00005, patience=20, verbose=1, mode='auto', baseline=None, restore_best_weights=True)
     model.fit(X_train, y_train, epochs=400, batch_size=100, validation_split=0.1, verbose=1, shuffle=True, callbacks=[reduce_lr, early_stopping])
     model.save("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\kerasmodel")
+
+def CNN():
+
+    data = Input(shape=(540, 960, 3), name="data")
+
+    conv1 = Conv2D(96, 11, name="conv1",  
+                strides=4, 
+                padding='same',
+                kernel_initializer=keras.initializers.RandomNormal(stddev=0.01), 
+                bias_initializer=keras.initializers.Constant(value=0),
+                kernel_regularizer=keras.regularizers.l2(1),
+                bias_regularizer=keras.regularizers.l2(0))(data)
+
+    relu1 = Activation("relu", name="relu1")(conv1)
+
+    pool1 = MaxPooling2D(pool_size=3, strides=2, name="pool1")(relu1)
+
+    norm1 = BatchNormalization(name="norm1")(pool1)
+
+
+    normalized1_1 = Lambda(lambda x: x[:, :, :, :48])(norm1)
+
+    normalized1_2 = Lambda(lambda x: x[:, :, :, 48:])(norm1)
+
+    conv2_1 = Conv2D(filters=128,
+                     kernel_size=5,
+                     activation='relu',
+                     strides=2,
+                     padding='same',
+                     kernel_initializer=keras.initializers.RandomNormal(stddev=0.01), 
+                     bias_initializer=keras.initializers.Constant(value=1),
+                     kernel_regularizer=keras.regularizers.l2(1),
+                     bias_regularizer=keras.regularizers.l2(0),
+                     name='conv2_1')(normalized1_1)
+
+    conv2_2 = Conv2D(filters=128,
+                     kernel_size=5,
+                     activation='relu',
+                     strides=2,
+                     padding='same',
+                     kernel_initializer=keras.initializers.RandomNormal(stddev=0.01), 
+                     bias_initializer=keras.initializers.Constant(value=1),
+                     kernel_regularizer=keras.regularizers.l2(1),
+                     bias_regularizer=keras.regularizers.l2(0),
+                     name='conv2_2')(normalized1_2)
+
+    conv2 = Concatenate(name='conv_2_merge')([conv2_1, conv2_2])
+
+    relu2 = Activation("relu", name="relu2")(conv1)
+
+    pool2 = MaxPooling2D(pool_size=3, strides=2, name="pool2")(relu2)
+
+    norm2 = BatchNormalization(name="norm2")(pool2)
+
+    conv3 = Conv2D(384, 3, name="conv3",  
+            strides=1, 
+            kernel_initializer=keras.initializers.RandomNormal(stddev=0.01), 
+            bias_initializer=keras.initializers.Constant(value=0),
+            kernel_regularizer=keras.regularizers.l2(1),
+            bias_regularizer=keras.regularizers.l2(0))(norm2)
+
+    relu3 = Activation("relu", name="relu3")(conv3)
+
+    relu3_1 = Lambda(lambda x: x[:, :, :, :192])(relu3)
+
+    relu3_2 = Lambda(lambda x: x[:, :, :, 192:])(relu3)
+
+    conv4_1 = Conv2D(filters=192,
+                     kernel_size=3,
+                     activation='relu',
+                     strides=1,
+                     padding='same',
+                     kernel_initializer=keras.initializers.RandomNormal(stddev=0.01), 
+                     bias_initializer=keras.initializers.Constant(value=1),
+                     kernel_regularizer=keras.regularizers.l2(1),
+                     bias_regularizer=keras.regularizers.l2(0),
+                     name='conv4_1')(relu3_1)
+
+    conv4_2 = Conv2D(filters=192,
+                     kernel_size=3,
+                     activation='relu',
+                     strides=1,
+                     padding='same',
+                     kernel_initializer=keras.initializers.RandomNormal(stddev=0.01), 
+                     bias_initializer=keras.initializers.Constant(value=1),
+                     kernel_regularizer=keras.regularizers.l2(1),
+                     bias_regularizer=keras.regularizers.l2(0),
+                     name='conv4_2')(relu3_2)
+
+    conv4 = Concatenate(name='conv_4_merge')([conv4_1, conv4_2])
+
+    relu4 = Activation("relu", name="relu4")(conv4)
+
+    relu4_1 = Lambda(lambda x: x[:, :, :, :128])(relu3)
+
+    relu4_2 = Lambda(lambda x: x[:, :, :, 128:])(relu3)
+
+    conv5_1 = Conv2D(filters=128,
+                     kernel_size=3,
+                     activation='relu',
+                     strides=1,
+                     padding='same',
+                     kernel_initializer=keras.initializers.RandomNormal(stddev=0.01), 
+                     bias_initializer=keras.initializers.Constant(value=1),
+                     kernel_regularizer=keras.regularizers.l2(1),
+                     bias_regularizer=keras.regularizers.l2(0),
+                     name='conv5_1')(relu3_1)
+
+    conv5_2 = Conv2D(filters=128,
+                     kernel_size=3,
+                     activation='relu',
+                     strides=1,
+                     padding='same',
+                     kernel_initializer=keras.initializers.RandomNormal(stddev=0.01), 
+                     bias_initializer=keras.initializers.Constant(value=1),
+                     kernel_regularizer=keras.regularizers.l2(1),
+                     bias_regularizer=keras.regularizers.l2(0),
+                     name='conv5_2')(relu3_2)
+
+    conv5 = Concatenate(name='conv_5_merge')([conv5_1, conv5_2])
+
+
+    relu5 = Activation("relu", name="relu5")(conv5)
+
+    pool5 = MaxPooling2D(pool_size=3, strides=2, name="pool5")(relu5)
+
+    fc6 = Dense(4096,
+                     kernel_initializer=keras.initializers.RandomNormal(stddev=0.005), 
+                     bias_initializer=keras.initializers.Constant(value=1),
+                     kernel_regularizer=keras.regularizers.l2(1),
+                     bias_regularizer=keras.regularizers.l2(0),
+                     name="fc6")(pool5)
+
+    relu6 = Activation("relu", name="relu6")(fc6)
+
+    fc7 = Dense(4096,
+                     kernel_initializer=keras.initializers.RandomNormal(stddev=0.005), 
+                     bias_initializer=keras.initializers.Constant(value=1),
+                     kernel_regularizer=keras.regularizers.l2(1),
+                     bias_regularizer=keras.regularizers.l2(0),
+                     name="fc7")(relu6)
+
+    relu7 = Activation("relu", name="relu7")(fc7)
+
+    fc8 = Flatten()(relu7)
+    pred = Dense(units=9, use_bias=False, name="pred")(fc8)
+
+    model = Model(inputs=[data], outputs=[pred])
+    model.summary()
+    model.compile(loss='mse',
+                   optimizer='adam',
+                   metrics=['accuracy', R_squared])
+    
+    X_train = np.fromfile("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\frames_full", dtype=np.dtype("(540, 960, 3)u1"))
+    Y_train = np.fromfile("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\boxes_full", dtype=np.dtype("(9,)f8"))
+
+    reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_R_squared', verbose=1, patience=8, mode='max')
+    early_stopping = keras.callbacks.EarlyStopping(monitor='val_R_squared', min_delta=0.0002, patience=15, verbose=1, mode='max', baseline=None, restore_best_weights=True)
+
+    model.fit(X_train, Y_train, epochs=80, batch_size=10, validation_split=0.1, verbose=1, shuffle=True, callbacks=[reduce_lr, early_stopping])
+    model.save("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\kerasmodel")
+
 
 def TrainSegmentationWithGenerator():
     model = keras.Sequential()
@@ -458,12 +613,12 @@ def TrainSegmentationWithGenerator():
 def ValidateSegmentation():
     model = []
     with CustomObjectScope({'R_squared': R_squared}):
-        model = load_model("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\kerasmodel")
+        model = load_model("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\kerasmodelBackup")
 
-    scalerX = load('C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\std_scalerx_segmentation.bin')
-    scalerY = load('C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\std_scalery_segmentation.bin')
+    #scalerX = load('C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\std_scalerx_segmentation.bin')
+    #scalerY = load('C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\Images\\std_scalery_segmentation.bin')
 
-    for r, d, f in os.walk("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\sampleDataset\\input_sample\\background00"):
+    for r, d, f in os.walk("C:\\Users\\jxmr\\Desktop\\ProjectIII\\OCRDataset\\Segmentation\\testDataset\\background05"):
         for file in f:
             if file.endswith(".avi"):
                 p = os.path.join(r, file)
@@ -473,9 +628,10 @@ def ValidateSegmentation():
                 while success:
                     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
                     resize = cv2.resize(gray, (int(gray.shape[1]/2), int(gray.shape[0]/2)), interpolation= cv2.INTER_LANCZOS4)
-                    array = np.asarray(resize).reshape(518400).reshape(1, -1).astype(np.float64)
-                    test_predictions = model.predict(scalerX.transform(array))
-                    prediction = scalerY.inverse_transform(test_predictions[0])
+                    array = np.asarray(resize).reshape(1,540,960,1).astype(np.float)
+                    test_predictions = model.predict(array)
+                    #test_predictions = model.predict(scalerX.transform(array))
+                    prediction = test_predictions[0]
                     prediction[1:] = prediction[1:] * 2
                     print(prediction)
                     pts = np.array([[prediction[1], prediction[2]],[prediction[3], prediction[4]],[prediction[5], prediction[6]],[prediction[7], prediction[8]]], np.int32)
@@ -604,6 +760,107 @@ def Replicate():
 
         plt.imshow(np.asarray(img_result2), cmap='gray')
         plt.show()
+
+
+class LRMultiplier(tf.keras.optimizers.Optimizer):
+
+    def __init__(self,
+                 optimizer,
+                 multipliers,
+                 **kwargs):
+        """Initialize the optimizer wrapper.
+        :param optimizer: The original optimizer.
+        :param multipliers: A dict representing the multipliers.
+                            The key is the prefix of the weight to be multiplied.
+        :param kwargs: Arguments for parent class.
+        """
+        super(LRMultiplier, self).__init__(**kwargs)
+        self.optimizer = tf.keras.optimizers.get(optimizer)
+        self.multipliers = multipliers
+        if hasattr(self.optimizer, 'learning_rate'):
+            self.lr_attr = 'learning_rate'
+        else:
+            self.lr_attr = 'lr'
+
+    @property
+    def lr(self):
+        return self.optimizer.lr
+
+    @lr.setter
+    def lr(self, lr):
+        self.optimizer.lr = lr
+
+    @property
+    def learning_rate(self):
+        return self.optimizer.learning_rate
+
+    @learning_rate.setter
+    def learning_rate(self, learning_rate):
+        try:
+            self.optimizer.learning_rate = learning_rate
+        except ValueError:
+            self.optimizer._hyper['learning_rate'] = learning_rate
+
+    def _get_multiplier(self, name):
+        multiplier, prefix_len = 1.0, 0
+        for key, val in self.multipliers.items():
+            if name.startswith(key):
+                if len(key) > prefix_len:
+                    prefix_len = len(key)
+                    multiplier = val
+        return multiplier
+
+    def get_updates(self, loss, params):
+        if len(self.updates) > 0:
+            return self.updates
+        multiplies = {}
+        for param in params:
+            multiplier = self._get_multiplier(param.name)
+            if multiplier not in multiplies:
+                multiplies[multiplier] = []
+            multiplies[multiplier].append(param)
+
+        self.updates, self.weights = [], []
+        origin_lr = getattr(self, self.lr_attr)
+        for i, (multiplier, params) in enumerate(multiplies.items()):
+            lr = origin_lr
+            if callable(multiplier):
+                lr = lr * multiplier(tf.keras.backend.cast(self.optimizer.iterations, K.floatx()))
+            elif multiplier != 1.0:
+                lr = lr * multiplier
+            setattr(self, self.lr_attr, lr)
+            with tf.keras.backend.name_scope('Group_{}'.format(i)):
+                self.updates += self.optimizer.get_updates(loss, params)
+            print(self.multipliers, i, self.optimizer.weights)
+            for w in self.optimizer.weights:
+                if w not in self.weights:
+                    self.weights.append(w)
+        setattr(self, self.lr_attr, origin_lr)
+
+        return self.updates
+
+    def get_config(self):
+        config = {
+            'optimizer': tf.keras.optimizers.serialize(self.optimizer),
+            'multipliers': self.multipliers
+        }
+        base_config = super(LRMultiplier, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def _create_slots(self, var_list):
+        for v in var_list:
+            self.add_slot(v, v.name[:-2])
+        
+    def _resource_apply_dense(self, grad, handle):
+        return
+
+    def _apply_dense(self, grad, var):
+        return
+
+    @classmethod
+    def from_config(cls, config):
+        optimizer = tf.keras.optimizers.deserialize(config.pop('optimizer'))
+        return cls(optimizer, **config)
 
 if __name__ == '__main__':
     ValidateSegmentation()
